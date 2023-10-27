05fc9dd201f6d7d0ae27e5c9a5c46e1ad52a9af2
ADR0407 <97013064+ADR0407@users.noreply.github.com>
2023-10-28 01:21:24 +0530

    Update hello_world.ts

--- src/client/hello_world.ts
+++ src/client/hello_world.ts
@@ -90,6 +90,9 @@ export async function establishConnection(): Promise<void> {
   // Step 1: Connect to the Solana Devnet
   /* Get the connection object and retrieve the 
    version from the connection object. */
+connection = new Connection("http://127.0.0.1:8899","confirmed");
+const version = await connection.getVersion();
+console.log('Connection to cluster established:', version);
 
   //Insert the Step 1 code from the tutorial here
 }
@@ -100,10 +103,17 @@ export async function establishConnection(): Promise<void> {
 export async function establishPayer(): Promise<void> {
   
   //Step 2: Generate a keypair - this would be an account that pays for the calls to the program
-  
+payer = Keypair.generate();
+console.log("Public Key of Payer is:", payer.publicKey.toString());
   //Insert the Step 2 code from the tutorial here
   
   //Step 3: Requesting an airdrop
+  const sig = await connection.requestAirdrop(
+  payer.publicKey,
+  2 * LAMPORTS_PER_SOL,
+);
+
+await connection.confirmTransaction(sig);
 
    //Insert the Step 3 code from the tutorial here
    
